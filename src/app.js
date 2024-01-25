e9be3b64474c2df9bd8d85d382aa63c699fe8b73
Danny Govantes <dannygovantes@gmail.com>
2024-01-24 21:15:58 -0600

    [ADD]setup for project

---
+++ src/app.js
@@ -0,0 +1,8 @@
+function ButtonState(lectura) {
+  if (lectura > 1) return;
+}
+// En todos los casos, el parámetro “lectura” es un entero sin signo
+//  y es modificado dentro de la función “ButtonState”, contiene el valor de
+// lectura “1” cuando el botón no se ha detectado como apretado y el valor de lectura “0”
+// cuando se ha detectado como apretado, cualquier valor de lectura arriba de “1” significa
+// que la lectura no ha podido ejecutarse.
